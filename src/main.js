f636278ca275b67282e8f28fcedf805383f96b82
YeonjiLim <reusepaper210@gmail.com>
2019-07-23 18:00:08 +0900

    yeon|full-page

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
+import VueFullPage from 'vue-fullpage.js'
 
 Vue.config.productionTip = false
+Vue.use(VueFullPage);
 
 new Vue({
   store,
