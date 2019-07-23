7cf94b833c7868ecec51c6744cbf7e09bdfc15b0
youdongkwan <dkyou7@naver.com>
2019-07-23 17:41:18 +0900

    YDK| banner-container-border-init

--- src/components/Main/BannerContain.vue
+++ src/components/Main/BannerContain.vue
@@ -1,5 +1,7 @@
 <template>
-
+  <div class="app">
+    this is BannerContain Main section
+  </div>
 </template>
 
 <script>
@@ -8,6 +10,9 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+  .app{
+    height: 96vh;
+    border:3px #f99 solid;
+  }
 </style>
