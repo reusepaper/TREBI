b14a3525e93bf9056732d177cf497b8fbc6b77f0
sweetrain096 <gpfhddl09@gmail.com>
2019-07-23 16:25:29 +0900

    featuer/login version1

---
+++ src/components/Main/AuthContain.vue
@@ -0,0 +1,18 @@
+<template>
+    <Login></Login>
+</template>
+
+<script>
+import Login from "./AuthComponents/Login";
+
+export default {
+    components: {
+        Login
+    }
+
+}
+</script>
+
+<style>
+
+</style>
