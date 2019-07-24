0bd999264a1f50a7d0e97e49109213e9123a9e36
sweetrain096 <gpfhddl09@gmail.com>
2019-07-24 14:39:51 +0900

    [#3] header 추가

---
+++ src/components/Main/BannerComponents/BannerHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1 id="Header">TREBI</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  #Header {
+    margin-left: 3%;
+    padding: 0px;
+    color: rgb(81, 122, 83);
+    /* font-size: 250px; */
+    font-size: 16em;
+    position: relative;
+    top: 40%;
+  }
+</style>
