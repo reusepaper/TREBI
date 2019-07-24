032926b70b631c241d55d8dabe1e66edfb3e9d7e
sweetrain096 <gpfhddl09@gmail.com>
2019-07-24 12:08:36 +0900

    [#3] grid 수정

---
+++ src/components/Main/MainContentContain.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="main-content">
+    <h1>Main Content</h1>
+    <MainContent></MainContent>
+  </div>
+</template>
+
+<script>
+import MainContent from './MainContentComponents/MainContent'
+export default {
+  components: {
+    MainContent
+  }
+}
+</script>
+
+<style>
+
+</style>
