9e1f4281b62cce0ccf24e05b9980ee74bdbae658
leeiopd <leeiopd@hanmail.net>
2019-07-29 09:51:39 +0900

    Develop|structure 변경

--- README.md
+++ README.md
@@ -36,26 +36,74 @@ See [Configuration Reference](https://cli.vuejs.org/config/).
 
 ## 기획
 
+### Page
+
 1. Main
+
    - 베너
    - 로그인 기능
+
 2. Team 소개
+
    - 사진
    - 내용
+
+   ***
+
+   동관
+
 3. Member
+
    - LG 페이지 참고 - http://www.lgchallengers.com/lg-social-challenger-2019/
    - 내용
      - 추후 추가
+
 4. Post
+
    - 멤버 사진으로 개인 Post 접근 - LG 멤버소개 페이지 참고 http://www.lgchallengers.com/lg-social-challenger-2019/
    - 개인 별 post list - 톨게이트 FAQ 페이지 참고 http://www.tollgateapp.com/main/index.php#FAQ
    - post 내용 - 인스타그램 모달 참고
+
+   ***
+
+   연지
+
 5. Post 작성 ( 관리자 로그인 시)
+
    - 현재 만들어둔 페이지 이용
+
+   ***
+
+   만섭
+
 6. Git graph
+
    - 개인별 github commit log 그래프 형식으로 뿌리기
+
 7. Footer
+
    - contact us
-   - 챗봇
    - rss
    - 날씨
+
+   ***
+
+   주호
+
+8. Sidebar
+
+   - +챗봇
+
+   ***
+
+   단비
+
+9. 관리자 페이지
+
+   - Firebase DB
+
+### 협업
+
+- GitFlow 방식 사용
+- rebase 를 통한 version up log
+- Jira 와 Issue 도입으로 테스크별 commit log 작성
--- src/Trebi.vue
+++ src/Trebi.vue
@@ -1,5 +1,6 @@
 <template>
   <full-page ref="fullpage" :options="options" id="fullpage">
+    <Header></Header>
     <div class="section">
       <Main></Main>
     </div>
@@ -12,21 +13,12 @@
       <Member></Member>
     </div>
     <div class="section">
-      <Posts></Posts>
-    </div>
-    <div class="section">
-      <PostWrite></PostWrite>
-    </div>
-    <div class="section">
       <div class="GraphPageContain">
         <GitGraph></GitGraph>
       </div>
     </div>
     <div class="section">
       <Footer></Footer>
-    </div>
-    <div class="section">
-      <Sidebar></Sidebar>
     </div>
     <!-- <Main></Main>
     <Team></Team>
@@ -42,11 +34,9 @@
 import Main from "./views/Main";
 import Team from "./views/Team";
 import Member from "./views/Member";
-import Posts from "./views/Posts";
-import PostWrite from "./views/PostsWrite";
 import GitGraph from "./views/GitGraph";
 import Footer from "./views/Footer";
-import Sidebar from "./views/Sidebar";
+import Sidebar from "./views/Header";
 
 export default {
   name: "Trebi",
@@ -54,51 +44,9 @@ export default {
     Main,
     Team,
     Member,
-    Posts,
-    PostWrite,
     GitGraph,
     Footer,
-    Sidebar
-  },
-  data() {
-    return {
-      options: {
-        licenseKey: "OPEN-SOURCE-GPLV3-LICENSE",
-        anchors: [
-          "page1",
-          "page2",
-          "page3",
-          "page4",
-          "page5",
-          "page6",
-          "page7",
-          "page8"
-        ],
-        sectionsColor: [
-          "#F7F7F7",
-          "#edffa4",
-          "#edffa4",
-          "#edffa4",
-          "#edffa4",
-          "#edffa4",
-          "#edffa4",
-          "#f7f7f7"
-        ]
-      }
-    };
+    Header
   }
 };
-</script>
-<style>
-.teampageContain {
-  height: 70%;
-  background-color: #f7f7f7;
-  transform: translate(0, 20%);
-}
-
-.GraphPageContain {
-  height: 70%;
-  background-color: #f7f7f7;
-  transform: translate(0, 20%);
-}
-</style>
+<style></style>;
