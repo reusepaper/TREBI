4fbf3d3ec1247ab4fc65634526f0f3612000b666
leeiopd <leeiopd@hanmail.net>
2019-07-29 12:18:16 +0900

    [#7] S1P2213002-61 Admin Sidebar
    - Admin Sidebar Title 구현

--- src/components/Admin/AdminSideBarContain.vue
+++ src/components/Admin/AdminSideBarContain.vue
@@ -1,10 +1,33 @@
 <template>
-  <p>사이드바</p>
+  <div class="sideBar">
+    <div style="margin: 20px; height: 100%;">
+      <AdminTitle></AdminTitle>
+      <AuthContain></AuthContain>
+      <PostContain></PostContain>
+      <Analistics></Analistics>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import AdminTitle from "./SidebarComponents/AdminTitle";
+import AuthContain from "./SidebarComponents/AuthContain";
+import PostContain from "./SidebarComponents/PostContain";
+import Analistics from "./SidebarComponents/Analistics";
+
+export default {
+  components: {
+    AdminTitle,
+    AuthContain,
+    PostContain,
+    Analistics
+  }
+};
 </script>
 
 <style>
+.sideBar {
+  height: 100vw;
+  background-color: #262f3d;
+}
 </style>
---
+++ src/components/Admin/SidebarComponents/AdminTitle.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <img class="sidebarLogo" src="../../../assets/admin.png" />
+    <span class="sidebarTitle">Trebi Admin</span>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.sidebarLogo {
+  width: 50px;
+  height: 50px;
+  float: left;
+}
+
+.sidebarTitle {
+  display: inline-block;
+  font-size: 25px;
+  color: #ffffff;
+}
+</style>
