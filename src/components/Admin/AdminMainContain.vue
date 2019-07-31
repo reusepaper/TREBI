a32fe158e333185db4f57f483ab1b1bcf265498b
leeiopd <leeiopd@hanmail.net>
2019-07-31 17:56:27 +0900

    [#24] S1P2213002-18 admin Post Page 구현
    - Firebase Database Post정보 불러오기 구현

--- src/components/Admin/AdminMainContain.vue
+++ src/components/Admin/AdminMainContain.vue
@@ -2,14 +2,17 @@
   <div>
     <p>메인</p>
     <mainTable></mainTable>
+    <PostList></PostList>
   </div>
 </template>
 
 <script>
-import mainTable from './MainComponents/AdminMainTable'
+import mainTable from "./MainComponents/AdminMainTable";
+import PostList from "./MainComponents/PostComponents/PostList";
 export default {
   components: {
     mainTable,
+    PostList
   }
 };
 </script>
