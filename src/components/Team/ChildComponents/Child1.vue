9ff8c2a5917602a77963bb6c29d75eeb8146c3a2
youdongkwan <dkyou7@naver.com>
2019-08-02 16:21:56 +0900

    [#32] S1P2213002-69 상세 페이지 구현 완료

---
+++ src/components/Team/ChildComponents/Child1.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="child1">
+    <h1>1주차 프로젝트</h1>
+    <ul>
+      <li>자신을 소개하는 포토폴리오 제작a</li>
+      <li>Vuetify를 이용한 Vue 학습</li>
+      <li>반응형 웹 사이트 구현</li>
+      <li>Git Repository를 이용한 Commit Log 남기기</li>
+    </ul>
+    <div class="cloo">
+      <a v-on:click="$modal.hide('hello-world')">close</a>
+    </div>
+  </div>
+</template> 
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+  .child1{
+    text-align: center
+  }
+  .child1 p{
+    padding: 3px;
+  }
+  .child1 h1{
+    margin : 10px;
+  }
+  ul li{
+    list-style: none;
+    padding : 5px;
+  }
+  .cloo{
+    position: absolute;
+    display: block;
+    font-size: 2em;
+    left: 42%;
+    top: 70%;
+    background: orange;
+    color: white;
+    border-radius: 10px;
+    padding: 10px 20px;
+    cursor:pointer;
+  }
+</style>
