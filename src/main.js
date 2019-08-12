90d053eb17589fb5c997ddaaebfede76853284b3
한만섭 <13akstjq@naver.com>
2019-08-12 10:32:20 +0900

    모바일 환경에서 커밋 그래프 숨김

--- src/main.js
+++ src/main.js
@@ -1,16 +1,17 @@
 import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
-
+import device from "vue-device-detector";
 // import VueFullPage from "vue-fullpage.js";
-import router from './router'
+import router from "./router";
 
-import './registerServiceWorker'
+import "./registerServiceWorker";
 
 // firebaseui css
 require("../node_modules/firebaseui/dist/firebaseui.js");
 require("../node_modules/firebaseui/dist/firebaseui.css");
 Vue.config.productionTip = false;
+Vue.use(device);
 // Vue.use(VueFullPage);
 
 new Vue({
